5f93ebaf5499e1c4e516feaec3e45f9d9d4b6590
hmygithub <2713237776@qq.com>
2017-09-19 15:16:14 +0800

    写博客

---
+++ builds.js
@@ -0,0 +1,14 @@
+
+
+const fs = require('fs')// file system
+const path = require('path')
+fs.readdir('./markdown', function(error, files){
+   for(var i = 0; i< files.length; i++){	
+	var p = path.join('./markdown',files[i])
+	var markdown = fs.readFileSync(p).toString()
+	var template = fs.readFileSync('./template.html').toString()
+	var result = template.replace('%content%',markdown)
+	fs.writeFileSync(files[i]+'.html', result)
+	console.log(result)
+}
+})
